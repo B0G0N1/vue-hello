a0845c7473486cfc2c8a484cc7d116d834d3972d
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-16 18:09:39 +0200

    [FEAT] Add comments

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+// Importa la funzione `createApp` da Vue
 const { createApp } = Vue;
 
+// Crea una nuova applicazione Vue
 createApp({
     data() {
         return {
+            // Messaggio di testo che sarà visualizzato nell'app
             message: 'Hello, Vue.js!',
-            imageUrl: 'https://vuejs.org/images/logo.png'
+            // URL di un'immagine che sarà visualizzata nell'app
+            imageUrl: 'https://notiziamix.it/wp-content/uploads/2023/02/Never-Gonna-Give-You-Up-Rick-Astley-Youtube.jpg'
         };
     }
 }).mount('#app');
